2b559a522ca06d39b38c825a2050adc8e5a09ead
Leo Eddy <leoeddy@Leos-MacBook-Pro.local>
2024-11-11 20:52:45 -0800

    Add touch test sketch

--- sketch.js
+++ sketch.js
@@ -1,33 +1,77 @@
+let block;
+let dragging = false;
+let offsetX, offsetY;
+
 function setup() {
   createCanvas(400, 400);
-  background(255);
-  textSize(16);
-  textAlign(CENTER, CENTER);
-  fill(0);
-  text("Touch the screen to test", width / 2, height / 2);
+  block = new Block(150, 150, 100, 60, 5); // Position and size of the block
 }
 
 function draw() {
-  // We’re not doing anything in draw, this is just for touch testing
+  background(220);
+
+  // Display the block
+  block.show();
+}
+
+function mousePressed() {
+  if (block.isHovered(mouseX, mouseY)) {
+    dragging = true;
+    offsetX = block.x - mouseX;
+    offsetY = block.y - mouseY;
+  }
+}
+
+function mouseDragged() {
+  if (dragging) {
+    block.x = mouseX + offsetX;
+    block.y = mouseY + offsetY;
+  }
+}
+
+function mouseReleased() {
+  dragging = false;
 }
 
 function touchStarted() {
-  background(200, 100, 100);
-  fill(255);
-  text("Touch Started", width / 2, height / 2);
-  return false; // Prevents any default behavior
+  if (block.isHovered(touchX, touchY)) {
+    dragging = true;
+    offsetX = block.x - touchX;
+    offsetY = block.y - touchY;
+  }
+  return false;
 }
 
 function touchMoved() {
-  background(100, 200, 100);
-  fill(255);
-  text("Touch Moved", width / 2, height / 2);
+  if (dragging) {
+    block.x = touchX + offsetX;
+    block.y = touchY + offsetY;
+  }
   return false;
 }
 
 function touchEnded() {
-  background(100, 100, 200);
-  fill(255);
-  text("Touch Ended", width / 2, height / 2);
+  dragging = false;
   return false;
+}
+
+// Block class
+class Block {
+  constructor(x, y, w, h, r) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.r = r;
+  }
+
+  show() {
+    fill(180);
+    stroke(0);
+    rect(this.x, this.y, this.w, this.h, this.r);
+  }
+
+  isHovered(px, py) {
+    return px > this.x && px < this.x + this.w && py > this.y && py < this.y + this.h;
+  }
 }
